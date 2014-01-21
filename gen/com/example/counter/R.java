58e0374ce05238d85cc99a15d8a41841a9f3a2e3
cntnboys <cpalexan@ualberta.ca>
2014-01-20 21:03:08 -0700

    Added in saving counter names into file :), into the listview

--- gen/com/example/counter/R.java
+++ gen/com/example/counter/R.java
@@ -38,7 +38,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main_counter=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int list_item=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/counter/Main.java
+++ src/com/example/counter/Main.java
@@ -1,8 +1,18 @@
 package com.example.counter;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -19,11 +29,15 @@ import android.widget.Toast;
 
 public class Main extends Activity {
 
-	final ArrayList<String> noteList = new ArrayList<String>();
+	ArrayList<String> noteList = new ArrayList<String>();
 	String selected = null;
-	
 	TextView Intro;
 	TextView Yourcounter;
+	
+	
+	private static final String FILENAME = "file3.sav";
+	private ListView myListview;
+	private EditText myEditText;
 	
 	
 	@Override
@@ -31,7 +45,7 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		//set intro message
+		//set message introduction
 		Intro = (TextView) findViewById(R.id.introText);
 		Intro.setText("Enter a name, press + to add counter");
 		
@@ -39,21 +53,20 @@ public class Main extends Activity {
 		Yourcounter.setText("Your Counters");
 		
 		//  List view element
-		final ListView myListView = (ListView)findViewById(R.id.counterlist);
-		
-		final EditText myEditText = (EditText)findViewById(R.id.countertext);
+	    myListview = (ListView)findViewById(R.id.counterlist);
+		myEditText = (EditText)findViewById(R.id.countertext);
 
-		final ArrayAdapter<String> aa;
+		//final ArrayAdapter<String> aa;
 
-		aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,noteList);
+		//aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,noteList);
 		
-		myListView.setAdapter(aa);
+		//myListView.setAdapter(aa);
 	
 
 		
 		//http://android.okhelp.cz/start-activity-from-listview-item-click-android-example/
 		 //list item click 
-	     myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	     myListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 	    	 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 	    		 
 	    		   
@@ -64,7 +77,7 @@ public class Main extends Activity {
 	              
 	               
 	              // http://stackoverflow.com/questions/801193/modify-view-static-variables-while-debugging-in-eclipse
-	               String selectedFromList =(String) (myListView.getItemAtPosition(position));
+	               String selectedFromList =(String) (myListview.getItemAtPosition(position));
 
 	               
 	               Intent intent = new Intent(Main.this,MainCounter.class);
@@ -78,18 +91,80 @@ public class Main extends Activity {
 		
 		//Button
 		Button btnSimple = (Button) findViewById(R.id.button1);
-		
 		btnSimple.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				noteList.add(0, myEditText.getText().toString());
-				aa.notifyDataSetChanged();
-				myEditText.setText("");
+				//noteList.add(0, myEditText.getText().toString());
+				//aa.notifyDataSetChanged();
+				//myEditText.setText("");
+				
+				setResult(RESULT_OK);
+				String text = myEditText.getText().toString();
+				saveInFile(text+"\n");
+				
 		   }
 	   });
 			
 	}
 	
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		List<String> tweets = loadFromFile();
+		
+		System.out.println(tweets);
+		
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+				R.layout.list_item, tweets);
+		myListview.setAdapter(aa);
+		
+	}
+
+	private List<String> loadFromFile() {
+		
+		List<String> tweets = new ArrayList<String>();
+		//System.out.println(tweets);
+		try {
+			FileInputStream fis = openFileInput(FILENAME);
+			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+			String line = in.readLine();
+			while (line != null) {
+				tweets.add(line);
+				//System.out.println(tweets);
+				line = in.readLine();
+			}
+			//System.out.println(tweets);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tweets;
+		//return tweets.toArray(new String[tweets.size()]);
+	}
+	
+	
+	
+	
+	private void saveInFile(String text) {
+		try {
+			FileOutputStream fos = openFileOutput(FILENAME,
+					Context.MODE_APPEND);
+			fos.write(new String(text)
+					.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
+	
